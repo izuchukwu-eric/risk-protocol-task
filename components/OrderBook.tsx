e149f4d85f92cc87b38a59d4d97cec8ef66c475b
izuchukwu-eric <onukwubeizu@gmail.com>
2023-03-09 16:06:34 +0100

    added optional chaining

--- components/OrderBook.tsx
+++ components/OrderBook.tsx
@@ -29,28 +29,28 @@ function OrderBook() {
             </thead>
             <tbody className="bg-black divide-y divide-gray-300">
               {state &&
-                state.asks.records.map((asks: any, idx: number) => (
-                  <tr key={asks.metaData.orderHash}>
+                state?.asks?.records.map((asks: any, idx: number) => (
+                  <tr key={asks?.metaData?.orderHash}>
                     <td className=" text-center bg-[rgba(36,174,100,0.5)]">
                       <p className="font-semibold text-sm text-white">
                         {formatNumber(
-                          (asks.order.makerAmount) /
-                          ((asks.order.takerAmount) / 1000000000000)
+                          (asks?.order?.makerAmount) /
+                          ((asks?.order?.takerAmount) / 1000000000000)
                         )}
                       </p>
                     </td>
                     <td className="text-center w-[50px] mr-2">
                       <p className="font-semibold text-sm text-[#20b26c]">
                         {formatNumber(
-                          (asks.order.takerAmount) /
-                          (asks.order.makerAmount) / 
-                          ((asks.order.takerAmount) /1000000000000)
+                          (asks?.order?.takerAmount) /
+                          (asks?.order?.makerAmount) / 
+                          ((asks?.order?.takerAmount) /1000000000000)
                         )}
                       </p>
                     </td>
                     <td className=" text-center bg-[#cc3939]">
                       <p className="font-semibold text-sm text-white">
-                        {formatNumber(asks.order.takerAmount)}
+                        {formatNumber(asks?.order?.takerAmount)}
                       </p>
                     </td>
                   </tr>
@@ -70,28 +70,28 @@ function OrderBook() {
             </thead>
             <tbody className=" bg-black divide-y divide-gray-300">
               {state &&
-                state.bids.records.map((bids: any) => (
-                    <tr key={bids.metaData.orderHash}>
+                state?.bids?.records.map((bids: any) => (
+                    <tr key={bids?.metaData?.orderHash}>
                     <td className=" text-center bg-[rgba(36,174,100,0.5)]">
                       <p className="font-semibold text-sm text-white">
                         {formatNumber(
-                          (bids.order.takerAmount) /
-                          ((bids.order.makerAmount) / 1000000000000)
+                          (bids?.order?.takerAmount) /
+                          ((bids?.order?.makerAmount) / 1000000000000)
                         )}
                       </p>
                     </td>
                     <td className="text-center w-[50px] mr-2">
                       <p className="font-semibold text-sm text-[#20b26c]">
                       {formatNumber(
-                          (bids.order.makerAmount) /
-                          (bids.order.takerAmount) / 
-                          ((bids.order.makerAmount) /1000000000000)
+                          (bids?.order?.makerAmount) /
+                          (bids?.order?.takerAmount) / 
+                          ((bids?.order?.makerAmount) /1000000000000)
                         )}
                       </p>
                     </td>
                     <td className=" text-center bg-[#cc3939]">
                       <p className="font-semibold text-sm text-white">
-                        {formatNumber(bids.order.makerAmount)}
+                        {formatNumber(bids?.order?.makerAmount)}
                       </p>
                     </td>
                   </tr>
